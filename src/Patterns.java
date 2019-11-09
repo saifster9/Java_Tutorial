7b35480d75a336a76a0afd1017de44b294d5fd97
saifs <saifs@Osman-Ws>
2019-11-09 15:16:05 -0600

    Updated the remaining (triangle w/ numbers, and letters and square) patterns and tested for a working solution

--- src/Patterns.java
+++ src/Patterns.java
@@ -1,12 +1,4 @@
-/**
- * Printing the following pattern:
- * 1
- * 12
- * 123
- * 1234
- * 12345
- * 123456
- */
+//Printing Patterns with Java
 
 /**
  * @author saifs
@@ -22,16 +14,73 @@ public class Patterns
 
 	public static void main(String[] args)
 	{
-		int line = 1;
 		
-		while (line <= 6)
+		/* 
+		 * Code to print the following pattern:
+		 * 1
+		 * 1 2
+		 * 1 2 3
+		 * 1 2 3 4 
+		 * 1 2 3 4 5
+		 * 1 2 3 4 5 6
+		 */
+		
+		for (int row=1; row<=6; row++) // Outer loop to track the number of rows
 		{
-			for (int i = 1; i <= line; i++)
+			for (int col=1; col<=row; col ++) // Inner loop to track the number of colums per row
 			{
-				System.out.print(i);
+				System.out.print(col + " "); // Printing numbers and spaces
+			}
+			System.out.println(); // Printing a new line at the end of each row
+		}
+		System.out.println();
+		
+		/* 
+		 * Code to print the following pattern:
+		 * A
+		 * A B
+		 * A B C
+		 */
+		
+		
+		for (int row=1; row<=3; row++)
+		{
+			char a = 'A';
+			for (int col=1; col<=row; col++)
+			{
+				
+				
+				System.out.print(a + " ");
+				a++;
 			}
 			System.out.println();
-			line++;
+			a = 65;
 		}
+		System.out.println();
+			
+			/* 
+			 * Code to print the following pattern:
+			 * $ $ $ $
+			 * $     $
+			 * $     $
+			 * $ $ $ $
+			 */
+		
+		for (int row=1; row<=4; row++)
+		{
+			char a = '$';
+			for (int col=1; col<=4; col++)
+			{				
+				if (col==1 | col==4 || row==1 || row==4)
+				{
+					System.out.print(a + " ");
+				}
+				else
+					System.out.print("  ");
+				
+			}
+			System.out.println();
+		}
+
 	}
 }
