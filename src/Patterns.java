e5205ead8bea154bf6a1e5c5c0f0d7097ef249cb
saifs <saifs@Osman-Ws>
2019-11-08 18:50:27 -0600

    FIrst Commit

---
+++ src/Patterns.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * @author saifs
+ *
+ */
+public class Patterns
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		for (int i = 1; i <= 6; i++)
+		{
+			System.out.print(i);
+			for (i = 1; i <= 2; i++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+
+		}
+
+	}
+
+}
